c8b14027b37bacb762e20f1f092b676978bc60c1
Jonas Jurczok <jonas.jurczok@akelius.de>
2019-03-20 17:20:27 +0100

    crazy test

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -12,7 +12,6 @@ export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf
 
 ./mvnw versions:set versions:commit -B -DnewVersion="2"
 
-git diff
 
 git config --global user.email "${GITHUB_ACTOR}@github-actions.com"
 git config --global user.name "$GITHUB_ACTOR"
