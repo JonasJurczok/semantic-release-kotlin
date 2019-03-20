a888dc69a2a5598cf26258a72dde08bceae9eb86
Jonas <jonasjurczok@gmail.com>
2019-03-20 17:25:48 +0100

    crazy test (#19)
    * crazy test

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -8,3 +8,6 @@ echo "Working on branch ${BRANCH}"
 export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 
 ./mvnw -B clean test jacoco:report coveralls:report -DrepoToken="${COVERALLS_TOKEN}"
+
+
+
