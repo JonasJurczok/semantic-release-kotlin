a066871568e8ab9cbbe27189306751df421da9fe
Jonas Jurczok <jonas.jurczok@akelius.de>
2019-03-20 08:57:59 +0100

    make mvn quiet

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -3,7 +3,7 @@
 
 git branch
 
-./mvnw -B clean verify
+./mvnw -B clean verify -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 cat /github/workspace/semantic-release-kotlin-cli/target/surefire-reports/*
 
 # TODO: coveralls support
