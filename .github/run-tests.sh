c3c485a46d3e569d53f7e0b0131be73306215b5e
Jonas Jurczok <jonas.jurczok@akelius.de>
2019-03-20 17:25:02 +0100

    crazy test

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -10,14 +10,4 @@ export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf
 ./mvnw -B clean test jacoco:report coveralls:report -DrepoToken="${COVERALLS_TOKEN}"
 
 
-./mvnw versions:set versions:commit -B -DnewVersion="2"
 
-
-git config --global user.email "${GITHUB_ACTOR}@github-actions.com"
-git config --global user.name "$GITHUB_ACTOR"
-
-git commit -am"prepare release 2"
-git tag 2
-
-git push --set-upstream origin fix-workflow
-git push --set-upstream origin --tags
