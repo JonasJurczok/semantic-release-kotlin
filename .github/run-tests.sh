b723cc28e508d532e000305b2462da1d8b660828
Jonas Jurczok <jonas.jurczok@akelius.de>
2019-03-20 10:01:20 +0100

    fix build

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -eu
 
 git branch
 
 ./mvnw -B clean verify -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
-cat /github/workspace/semantic-release-kotlin-cli/target/surefire-reports/*
+
+./mvnw coveralls:report -DrepoToken="${COVERALLS_TOKEN}"
 
 # TODO: coveralls support
