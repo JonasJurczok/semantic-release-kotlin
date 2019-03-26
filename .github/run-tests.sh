644cfb7056f7e6e8d944b4986e9c76839af68731
Jonas Jurczok <jonas.jurczok@akelius.de>
2019-03-26 13:38:53 +0100

    fix release script

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -6,7 +6,7 @@ BRANCH=$(git branch | grep "*")
 echo "Working on branch ${BRANCH}"
 
 export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
-#./mvnw -B clean test jacoco:report
+./mvnw -B clean test jacoco:report
 
 if [[ -n "${COVERALLS_TOKEN:-}" ]]; then
     echo "Found coveralls token. Sending coverage reports"
